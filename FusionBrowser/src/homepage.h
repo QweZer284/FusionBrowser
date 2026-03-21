7e5eea8c24e589f97081f336155bd5641b230e75
QweZer284 <bodtokbotok77@gmail.com>
2026-03-21 11:29:35 +0300

    Delete FusionBrowser directory

--- FusionBrowser/src/homepage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QString>
-
-class Database;
-class ThemeManager;
-class Settings;
-
-// Generates the new-tab homepage HTML string.
-QString generateHomepageHtml(Database* db, ThemeManager* theme, Settings* settings);
